a530fa99e65b1cfeaeae4e48a4543e395c572f8f
Mayra Cabrera <mcabrera1087@gmail.com>
2016-04-27 23:44:27 -0500

    Add a new version of raindrops exercise

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -6,25 +6,41 @@ const testVersion = 2
 
 func Convert(x int) string {
 	result := ""
-	for i := 1; i <= x; i++ {
+	primes := SearchPrimeNumbers(x)
+	for _, v := range primes[:] {
 		word := ""
-		if (float64(x) / float64(i)) == 1 {
-			switch i {
-			case 3:
-				word = "Pling"
-			case 5:
-				word = "Plang"
-			case 7:
-				word = "Plong"
-			}
-			result = result + word
+		switch v {
+		case 3:
+			word = "Pling"
+		case 5:
+			word = "Plang"
+		case 7:
+			word = "Plong"
 		}
+		result = result + word
 	}
 
 	if len(result) == 0 {
 		result = strconv.Itoa(x)
 	}
 	return result
+}
+
+func SearchPrimeNumbers(x int) []int {
+	primes := []int{}
+	for i := 2; i <= x; i++ {
+		counting := 0
+		for j := 2; j <= i; j++ {
+			if (i % j) == 0 {
+				counting += 1
+				if (i == j) && counting == 1 {
+					primes = append(primes, i)
+				}
+			}
+		}
+
+	}
+	return primes
 }
 
 // The test program has a benchmark too.  How fast does your Convert convert?
