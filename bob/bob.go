4a8b76286db37b69074c6fde306782d90b71d60c
Mayra Cabrera <mcabrera1087@gmail.com>
2016-05-10 22:57:22 -0500

    Add solution for bob exercise

--- bob/bob.go
+++ bob/bob.go
@@ -1,3 +1,21 @@
 package bob
 
+import (
+	"strings"
+)
+
 const testVersion = 2
+
+func Hey(input string) string {
+	normalize_input := strings.TrimSpace(input)
+	switch {
+	case strings.ToUpper(normalize_input) == normalize_input && strings.ToLower(normalize_input) != strings.ToUpper(normalize_input):
+		return "Whoa, chill out!"
+	case strings.HasSuffix(normalize_input, "?"):
+		return "Sure."
+	case normalize_input == "":
+		return "Fine. Be that way!"
+	default:
+		return "Whatever."
+	}
+}
